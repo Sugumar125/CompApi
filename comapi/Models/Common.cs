a9dbde0fddfcefbb65bdc88ed744a77174356b58
Sugumar125 <93989440+Sugumar125@users.noreply.github.com>
2021-11-09 18:41:12 +0530

    Uploaded

---
+++ comapi/Models/Common.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace comapi.Models
+{
+    public class Test
+    {
+      
+
+    }
+
+    
+}
